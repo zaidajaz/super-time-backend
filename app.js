9edb9f5f55d1918c33708fca98cd762fb08498bd
Zaid Ajaz <zaidajaz@Zaids-MacBook-Air.local>
2018-10-20 22:02:28 +0530

    added cors support

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ const express = require('express');
 const app = express();
 const bodyparser = require('body-parser');
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 app.use(bodyparser.json());
 
 var router = require('./src/app.routing');
