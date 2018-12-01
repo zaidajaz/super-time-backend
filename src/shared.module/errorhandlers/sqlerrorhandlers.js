7ae013e1aab38f65f0d42e9c08a1e79f7b01d2b4
Zaid Ajaz <zaidajaz@Zaids-MacBook-Air.local>
2018-12-01 16:10:54 +0530

    SQL Response will give a response evertime now

--- src/shared.module/errorhandlers/sqlerrorhandlers.js
+++ src/shared.module/errorhandlers/sqlerrorhandlers.js
@@ -2,11 +2,11 @@ exports.handleSqlError = function(sqlResObj) {
     if(sqlResObj.sqlErr) {
         console.log(sqlResObj.sqlErr);
        return {valid: false, messages: [sqlResObj.sqlErr.code], response: null};
-    }
-    if(sqlResObj.conErr) {
+    } else if(sqlResObj.conErr) {
         return {valid: false, messages: ['Connection Error'], response: null};
-    }
-    if(sqlResObj.rows) {
+    } else if(sqlResObj.rows) {
         return {valid: true, messages: ['Success'], response: sqlResObj.rows};
+    } else {
+        return {valid: false, messages: ['Something Went Wrong'], response: null};
     }
 }
