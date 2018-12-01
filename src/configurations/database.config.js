1a6a63c91aa8c8d64b8de61021123204cc05590b
Zaid Ajaz <zaidajaz@Zaids-MacBook-Air.local>
2018-12-01 14:16:22 +0530

    Connection Lost Bug Handled

--- src/configurations/database.config.js
+++ src/configurations/database.config.js
@@ -8,26 +8,24 @@ var mysql = require('mysql')
 //   multipleStatements: true
 // });
 
-const connection = mysql.createPool({
-  connectionLimit: 10,
-  host     : '148.66.136.68',
-  port: '3306',
-  user     : 'zaid_forST',
-  password : 'diaz06041994',
-  database : 'super_time_app',
-  multipleStatements: true
-});
-
-connection.on("error", (err)=>console.log(err));
-
-// connection.connect()
-
-// connection.query('SELECT 1 + 1 AS solution', function (err, rows, fields) {
-//   if (err) throw err
-
-//   console.log('The solution is: ', rows[0].solution)
-// })
-
-// connection.end()
-
+var connection;
+function connectDB() {
+    connection = mysql.createPool({
+    connectionLimit: 10,
+    host     : '148.66.136.68',
+    port: '3306',
+    user     : 'zaid_forST',
+    password : 'diaz06041994',
+    database : 'super_time_app',
+    multipleStatements: true
+  });
+  
+  connection.on("error", (err)=>{
+    console.log(err);
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') {
+      connectDB();
+    }
+  });
+}
+connectDB();
 module.exports = connection;
