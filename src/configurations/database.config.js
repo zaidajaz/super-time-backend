45361b56254f9b28d14fef0cad5cba08e13d5fa8
Zaid Ajaz <zaidajaz@Zaids-MacBook-Air.local>
2018-12-01 10:51:13 +0530

    ready for initial deployment

--- src/configurations/database.config.js
+++ src/configurations/database.config.js
@@ -1,24 +1,24 @@
 var mysql = require('mysql')
-const connection = mysql.createPool({
-  connectionLimit: 10,
-  host     : 'localhost',
-  user     : 'root',
-  password : 'diaz06041994',
-  database : 'super_time_app',
-  multipleStatements: true
-});
-
 // const connection = mysql.createPool({
 //   connectionLimit: 10,
-//   host     : '148.66.136.68',
-//   port: '3306',
-//   user     : 'zaid_forST',
+//   host     : 'localhost',
+//   user     : 'root',
 //   password : 'diaz06041994',
 //   database : 'super_time_app',
 //   multipleStatements: true
 // });
 
+const connection = mysql.createPool({
+  connectionLimit: 10,
+  host     : '148.66.136.68',
+  port: '3306',
+  user     : 'zaid_forST',
+  password : 'diaz06041994',
+  database : 'super_time_app',
+  multipleStatements: true
+});
 
+connection.on("error", (err)=>console.log(err));
 
 // connection.connect()
 
