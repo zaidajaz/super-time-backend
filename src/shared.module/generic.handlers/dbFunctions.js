8d686fd30ce0ecc83f3876723ca0582221a5dcbe
Zaid Ajaz <zaidajaz@Zaids-MacBook-Air.local>
2018-12-01 15:34:07 +0530

    Connection Issue Handled

--- src/shared.module/generic.handlers/dbFunctions.js
+++ src/shared.module/generic.handlers/dbFunctions.js
@@ -4,16 +4,21 @@ exports.executeQuery=function(query,params,callback){
         if (err) {
         //   connection.release();
           callback({conErr: err});
-        }   
-        connection.query(query,params,function(err,rows){
-            connection.release();
-            if(!err) {
-                callback({rows: rows});
-            }    
-            else {
-                callback({sqlErr: err});  
-            }    
-        });
+        }  
+        if (connection) {
+            connection.query(query,params,function(err,rows){
+                connection.release();
+                if(!err) {
+                    callback({rows: rows});
+                }    
+                else {
+                    callback({sqlErr: err});  
+                }    
+            });
+        } 
+        else {
+            callback({connErr: 'Connection Issue, try again'});
+        }
         // connection.on('error', function(err) {      
         //       if(err.code === 'PROTOCOL_CONNECTION_LOST')  {
 
