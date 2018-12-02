3683439224ff3f9a441f753dfc0f627a924ffe1b
Zaid Ajaz <zaidajaz@Zaids-MacBook-Air.local>
2018-12-02 19:36:41 +0530

    Handled server crash

--- src/app.routing.js
+++ src/app.routing.js
@@ -1,6 +1,11 @@
 const express = require('express');
 var router = express.Router();
 
+process.on('uncaughtException', function (err) {
+    console.error(err);
+    console.log("Node NOT Exiting...");
+});
+
 var stockRouter = require('./stock.module/stock.routing');
 var customersRouter = require('./customers.module/customers.routing');
 var salesRouter = require('./sales.module/sales.routing');
